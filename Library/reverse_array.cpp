2525f14a2ca3edcd925a404df724c771a95b5aa9
minh711 <duyminhtruong711@gmail.com>
2023-12-05 07:28:24 +0700

    Reverse array

---
+++ Library/reverse_array.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+void a_reverse(int a[], int n);
+void a_display(int a[], int n);
+
+int main()
+{
+    int a[] = {8, 4, 2, 5, 2, 6, 3, 2};
+    int n = 8;
+    a_reverse(a, n);
+    a_display(a, 8);
+    return 0;
+}
+
+/**
+ * @brief Reversing an array.
+ *
+ * @param a The target array.
+ * @param n The array's length.
+ */
+void a_reverse(int a[], int n)
+{
+    int temp = 0;
+    for (int i = 0; i < (n / 2); i++)
+    {
+        temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+    }
+}
+
+/**
+ * @brief Display for an array.
+ *
+ * @param a Integer array.
+ * @param n Length of the array.
+ */
+void a_display(int a[], int n)
+{
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+    {
+        cout << " " << a[i];
+    }
+    cout << endl;
+}
